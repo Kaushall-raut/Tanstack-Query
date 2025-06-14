ec580b1e4c2f6e544f9cc1eb14de337fb3e2da04
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-06-14 20:52:10 +0530

    added dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { FetchOld } from "./pages/FetchOld";
 import { FetchRq } from "./pages/FetchRq";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const Routes = createBrowserRouter([
   {
@@ -31,9 +32,12 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       {
-          //like use context hook you also need to wrap it so other components can access its function 
+        //like use context hook you also need to wrap it so other components can access its function
       }
-
+      <TanStackRouterDevtools />{" "}
+      {
+        //this will activate the dev tool option on the browser which help to understand the working of the tanstack
+      }
       <RouterProvider router={Routes}>React Query</RouterProvider>
     </QueryClientProvider>
   );
